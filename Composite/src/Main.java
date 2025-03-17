362c63a3802cb469868e6750c19bcd60e3bb94ed
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2025-03-17 16:51:28 +0900

    Update Main.java - 띄어쓰기 변경

--- Composite/src/Main.java
+++ Composite/src/Main.java
@@ -4,12 +4,12 @@ public class Main {
         inventory.add(new NormalItem("칼"));
         inventory.add(new NormalItem("방패"));
 
-        StroageItem myBag = new StroageItem("물약가방");
+        StroageItem myBag = new StroageItem("물약 가방");
         inventory.add(myBag);
-        myBag.add(new NormalItem("회복물약"));
-        myBag.add(new NormalItem("마나물약"));
+        myBag.add(new NormalItem("회복 물약"));
+        myBag.add(new NormalItem("마나 물약"));
 
-        StroageItem specialBag = new StroageItem("스페셜가방");
+        StroageItem specialBag = new StroageItem("스페셜 가방");
         specialBag.add(new NormalItem("비법서"));
         specialBag.add(new NormalItem("다이아몬드"));
         inventory.add(specialBag);
