8077d027f7064dccc1bf6992c0580f783913ccab
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2025-03-17 18:28:53 +0900

    Update UserController.java
    주석 수정

--- MVC/src/UserController.java
+++ MVC/src/UserController.java
@@ -1,7 +1,7 @@
 public class UserController {
     private UserModel model;
     private UserView view;
-    // 중재자 역할을 위해 모델과 뷰를 멤버변수로 가지고 있습니다.
+    // 중재자 역할을 위해 모델과 뷰를 멤버변수로 보유
     public UserController(UserModel model, UserView view) {
         this.model = model;
         this.view = view;
@@ -15,7 +15,7 @@ public class UserController {
         model.setGrade(grade);
     }
 
-    public void updateView() { //모델이 수정되면 연관 뷰도 수정되도록 컨트롤러에서 제어 합니다.
+    public void updateView() { //모델이 수정되면 연관 뷰도 수정되도록 컨트롤러에서 제어
         view.displayUserInfo(model.getId(), model.getName(), model.getGrade());
     }
 }
